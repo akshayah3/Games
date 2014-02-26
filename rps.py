01c345c2035e6b49a6ad8ea6d8b5c55102bdba35
akshayah3 <akshaynukala95@gmail.com>
2014-02-27 00:28:39 +0530

    Docstring

--- rps.py
+++ rps.py
@@ -8,7 +8,9 @@ import random
 
 class Rps():
     """
-    A simple game of rock papers and scissors
+    A simple game of rock papers and scissors.Rock-paper-scissors is a hand game usually played by two people,
+    where players simultaneously form one of three shapes with an outstretched hand. The "rock" beats scissors,
+     the "scissors" beat paper and the "paper" beats rock; if both players throw the same shape, the game is tied.
 
     """
     def __init__(self):
