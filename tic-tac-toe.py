0d28a4b02b8f1850f8e26a1b52d1fce88153d381
akshayah3 <akshaynukala95@gmail.com>
2014-03-08 03:24:34 +0530

    some additions

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -4,6 +4,16 @@ class Tictactoe:
     """
     A simple game of Tictactoe
     """
+    Fi = ['first', None, None, None]
+    Se = ['second', None, None, None]
+    Th = ['third', None, None, None]
+    moves = ['o', 'x']
+    playermoves = 0
+    computermoves = 0
+    def __init__(self):
 
 
 
+
+    def player_move(self, playermoves, computermoves):
+        row = raw_input('which row do you want to play?')
