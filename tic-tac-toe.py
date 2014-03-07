54116c0b964d67134504479556f0ba5756b5a4b4
akshayah3 <akshaynukala95@gmail.com>
2014-03-08 03:46:08 +0530

    some additions

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -4,9 +4,10 @@ class Tictactoe:
     """
     A simple game of Tictactoe
     """
-    Fi = ['first', None, None, None]
-    Se = ['second', None, None, None]
-    Th = ['third', None, None, None]
+    Fi = [None, None, None]
+    Se = [None, None, None]
+    Th = [None, None, None]
+    structure = ['first', 'second', 'third']
     moves = ['o', 'x']
     playermoves = 0
     computermoves = 0
@@ -15,5 +16,21 @@ class Tictactoe:
 
 
 
-    def player_move(self, playermoves, computermoves):
+    def player_move(self):
         row = raw_input('which row do you want to play?')
+        if row in structure:
+            if self.row_validation(row):
+                column = raw_input('which column do you want to play?')
+                if self.column_valdation(column):
+
+
+    def computer_move(self):
+
+    def row_validation(self, row):
+
+
+    def column_valdation(self):
+
+
+
+    def result(self):
